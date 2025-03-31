3bca9e47b1664f813147b9609ae03281cbc2d35f
alvin <alvinsatria452@gmail.com>
2025-03-31 21:17:07 +0700

    Calculator finish

--- Kalkulator.java
+++ Kalkulator.java
@@ -3,4 +3,35 @@ public class Kalkulator {
     public static int add(int angka1, int angka2){
         return angka1 + angka2;
     }
+
+    public static int subtract(int angka1, int angka2){
+        return angka1 - angka2;
+    }
+
+    public int times(int angka1, int angka2){
+        return angka1 * angka2;
+    }
+
+    public int divide(int angka1, int angka2){
+        if(angka1 == 0 || angka2 == 0){
+            System.out.println("!!Tak terbatasssssss!!");
+            return 0;
+        } else {
+            return angka1 / angka2;
+        }
+    }
+
+    public int Sederhana(int pembilang, int penyebut) {
+        int fpb = cariFPB(pembilang, penyebut);
+        return pembilang / fpb;
+    }
+
+    private int cariFPB(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return Math.abs(a);
+    }
 }
--- PemdasPraktikumModul5.java
+++ PemdasPraktikumModul5.java
@@ -1,9 +1,20 @@
 public class PemdasPraktikumModul5 {
     public static void main(String[] args) {
 
-        int hasil = Kalkulator.add(99, 234);
+        int hasil1 = Kalkulator.add(99, 234);
+        int hasil2 = Kalkulator.subtract(420, 69);
 
-        System.out.println(hasil);
+        Kalkulator calc = new Kalkulator();
+
+        int hasil3 = calc.times(4, 7);
+        int hasil4 = calc.divide(300, 10);
+        int hasil5 = calc.Sederhana(18, 24);
+
+        System.out.println("Hasil penjumlahan: " + hasil1);
+        System.out.println("Hasil pengurangan: " + hasil2);
+        System.out.println("Hasil perkalian: " + hasil3);
+        System.out.println("Hasil pembagian: " + hasil4);
+        System.out.println("Hasil penyederhanaan: " + hasil5);
     }
 }
 
