6b4f4a66e6b97a9d598307d8df7d900b759826a6
alvin <alvinsatria452@gmail.com>
2025-03-31 18:31:45 +0700

    Progress calculator

--- Kalkulator.java
+++ Kalkulator.java
@@ -1,3 +1,6 @@
 public class Kalkulator {
 
+    public static int add(int angka1, int angka2){
+        return angka1 + angka2;
+    }
 }
--- PemdasPraktikumModul5.java
+++ PemdasPraktikumModul5.java
@@ -1,6 +1,9 @@
 public class PemdasPraktikumModul5 {
     public static void main(String[] args) {
 
+        int hasil = Kalkulator.add(99, 234);
+
+        System.out.println(hasil);
     }
 }
 
